990ca0dd2e2b5d553d7b4772ba21bb4fc06d87ac
Rafliramdhani16 <rafliramdhani773@gmail.com>
2024-11-21 18:23:38 +0700

    feat: membuat semua model tabel

--- app/Models/ShirtPhoto.php
+++ app/Models/ShirtPhoto.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ShirtPhoto extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'photo',
+        'shirt_id'
+    ];
 }
