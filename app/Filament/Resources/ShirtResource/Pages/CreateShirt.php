0abe6f0895b14153bf3c0ab28978c495e95a35c6
Rafliramdhani16 <rafliramdhani773@gmail.com>
2024-11-22 22:42:28 +0700

    pref: tabel database

---
+++ app/Filament/Resources/ShirtResource/Pages/CreateShirt.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ShirtResource\Pages;
+
+use App\Filament\Resources\ShirtResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateShirt extends CreateRecord
+{
+    protected static string $resource = ShirtResource::class;
+}
--- app/Models/Shirt.php
+++ app/Models/Shirt.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
 class Shirt extends Model
@@ -22,6 +23,7 @@ class Shirt extends Model
         'price',
         'stock',
         'is_popular',
+        'shirt_id',
         'category_id', //Fk
         'brand_id', //Fk 
     ];
@@ -39,5 +41,14 @@ class Shirt extends Model
     public function category():BelongsTo
     {
         return $this->belongsTo(Category::class, 'category_id');
+    }
+    public function photos(): HasMany
+    {
+        return $this->hasMany(ShirtPhoto::class, 'shirt_id');
+    }
+
+    public function sizes(): HasMany
+    {
+        return $this->hasMany(ShirtSize::class);
     }
 }
